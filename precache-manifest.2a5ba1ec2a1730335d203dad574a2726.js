7c512430ef510c0b7771a2d634dd3919f83e5d4c
Kitokitola <tetiana.la21@gmail.com>
2020-04-18 05:26:43 +0300

    Updates

---
+++ precache-manifest.2a5ba1ec2a1730335d203dad574a2726.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30c072c17b231a7ed2613834254ca71",
+    "url": "/react-essentials-15-hw/index.html"
+  },
+  {
+    "revision": "92420daa09144a58c1ee",
+    "url": "/react-essentials-15-hw/static/css/main.f236a158.chunk.css"
+  },
+  {
+    "revision": "1ea5b7be977bd65d6d6f",
+    "url": "/react-essentials-15-hw/static/js/2.300f543b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-essentials-15-hw/static/js/2.300f543b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92420daa09144a58c1ee",
+    "url": "/react-essentials-15-hw/static/js/main.a2937e0a.chunk.js"
+  },
+  {
+    "revision": "05b1746e880583e806ce",
+    "url": "/react-essentials-15-hw/static/js/runtime-main.19f4555c.js"
+  },
+  {
+    "revision": "78400896bd49415ed523bfac78a23324",
+    "url": "/react-essentials-15-hw/static/media/comment-icon.78400896.png"
+  },
+  {
+    "revision": "92f0193e24c870060bcce1a5745ef3c4",
+    "url": "/react-essentials-15-hw/static/media/down-arrow.92f0193e.png"
+  },
+  {
+    "revision": "82d568b2ea1b7a61b620f6c966be83d2",
+    "url": "/react-essentials-15-hw/static/media/like-icon.82d568b2.png"
+  },
+  {
+    "revision": "d96eca86cfdded0fbe037f084b525909",
+    "url": "/react-essentials-15-hw/static/media/repost-icon.d96eca86.png"
+  },
+  {
+    "revision": "d1608214d99af0004ba6cd21ff05f2e4",
+    "url": "/react-essentials-15-hw/static/media/retweet-icon.d1608214.png"
+  },
+  {
+    "revision": "dfc0747df4e9eeb50e28288995ed0709",
+    "url": "/react-essentials-15-hw/static/media/verified.dfc0747d.png"
+  }
+]);
